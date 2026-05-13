c14c9f066d6c0de743e685552cadf0f763c950fa
AxelBossMan <axwoxen@gmail.com>
2026-05-13 17:20:15 +0200

    Update photos.js

--- routes/photos.js
+++ routes/photos.js
@@ -1,19 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+const { requireUser } = require('../middleware/authMiddleware');
+const photoController = require('../controllers/photoController');
+
+router.get('/upload', requireUser, photoController.showUploadPage);
+
 router.post(
-    '/upload',
-    requireUser,
-    (req, res, next) => {
-      photoController.upload.single('photo')(req, res, (err) => {
-        if (err) {
-          return res.status(400).render('error', {
-            title: 'Upload failed',
-            message: err.message
-          });
-        }
-  
-        next();
-      });
-    },
-    photoController.handleUpload
+  '/upload',
+  requireUser,
+  (req, res, next) => {
+    photoController.upload.single('photo')(req, res, (err) => {
+      if (err) {
+        return res.status(400).render('error', {
+          title: 'Upload failed',
+          message: err.message
+        });
+      }
+
+      next();
+    });
+  },
+  photoController.handleUpload
 );
 
 router.post('/delete/:photoId', requireUser, photoController.deletePhoto);
+
+module.exports = router;
